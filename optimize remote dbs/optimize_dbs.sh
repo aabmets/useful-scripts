f6af5f601b89efa3f1b1048994d6cc50f900cc65
Mattias Aabmets <mattias.aabmets@gmail.com>
2022-04-24 12:21:51 +0300

    update

--- optimize remote dbs/optimize_dbs.sh
+++ optimize remote dbs/optimize_dbs.sh
@@ -1,30 +1,52 @@
 #!/bin/bash
-
-
-# -------------------------------------------------------------------------------- #
+# ================================================================================== #
+#                                                                                    #
+#   MIT License                                                                      #
+#                                                                                    #
+#   Copyright (c) 2022 Mattias Aabmets                                               #
+#                                                                                    #
+#   Permission is hereby granted, free of charge, to any person obtaining a copy     #
+#   of this software and associated documentation files (the "Software"), to deal    #
+#   in the Software without restriction, including without limitation the rights     #
+#   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        #
+#   copies of the Software, and to permit persons to whom the Software is            #
+#   furnished to do so, subject to the following conditions:                         #
+#                                                                                    #
+#   The above copyright notice and this permission notice shall be included in all   #
+#   copies or substantial portions of the Software.                                  #
+#                                                                                    #
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       #
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         #
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      #
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           #
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    #
+#   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE    #
+#   SOFTWARE.                                                                        #
+#                                                                                    #
+# ================================================================================== #
 #  Define settings.
 
 hosts_file="hosts.ini"
-remote_file="optimize_dbs.py"
+python_file="optimize_dbs.py"
 remote_path="/users/admin/scripts/"
 
 
-# -------------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------------  #
 #  Define constants.
 
 SCRIPT_VERSION="1.0.0"
-LOCAL_DEPS=("putty" "putty-tools")
+LOCAL_DEPS=("putty" "putty-tools" "parallel")
 REMOTE_DEPS=("python3" "python-is-python3")
 
 
-# ================================================================================ #
+# ================================================================================== #
 echo_title()
 {
   echo "--------------------------------"
   echo "OPTIMIZE REMOTE DATABASES SCRIPT"
 }
 
-# -------------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------------  #
 echo_help()
 {
   echo_title
@@ -34,23 +56,23 @@ echo_help()
   echo "python script named 'optimize_dbs.py' to each remote host and"
   echo "execute it with the args passed to this script."
   echo
-  echo "Syntax: optimize_dbs.sh [-n|h|V] [db_like_1 ...]"
+  echo "Syntax: optimize_dbs.sh [-n|V|D|h] [db_like_1 ...]"
   echo
   echo "positional params 'db_like_*':"
   echo "      Variable quantity of database names which to optimize."
   echo "      The database names can be in MySQL LIKE regex format."
   echo
   echo "options:"
-  echo "h     Print this help and exit."
-  echo "V     Print software version and exit."
-  echo "D     Print software dependencies and exit."
-  echo "n     Enables dry run mode, where the remote python scripts"
-  echo "      will return the names of the databases to be optimized"
-  echo "      without running any optimization on the databases."
+  echo "-h     Print this help and exit."
+  echo "-V     Print software version and exit."
+  echo "-D     Print software dependencies and exit."
+  echo "-n     Enables dry run mode, where the remote python scripts"
+  echo "       will return the names of the databases to be optimized"
+  echo "       without running any optimization on the databases."
   echo
 }
 
-# -------------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------------  #
 echo_deps()
 {
   echo_title
@@ -67,7 +89,7 @@ echo_deps()
   echo
 }
 
-# -------------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------------  #
 echo_version()
 {
   echo_title
@@ -76,7 +98,7 @@ echo_version()
   echo
 }
 
-# -------------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------------  #
 echo_usage()
 {
   echo "Run this script with the '-h' option to display the usage guide."
@@ -84,7 +106,7 @@ echo_usage()
 }
 
 
-# ================================================================================  #
+# ================================================================================== #
 #  Parse and evaluate the args.
 
 if [[ "$*" == "" ]]; then
@@ -111,7 +133,7 @@ while getopts ":nDVh" option; do
   esac
 done
 
-# --------------------------------------------------------------------------------  #
+# ---------------------------------------------------------------------------------  #
 #  Ensure all local dependencies are installed before continuing.
 
 for dep in ${LOCAL_DEPS[*]}; do
@@ -122,7 +144,7 @@ for dep in ${LOCAL_DEPS[*]}; do
 done
 
 
-# ================================================================================  #
+# ================================================================================== #
 #  The main worker function sanitizes the hostname and checks
 #  if the remote host is alive. If it is, then it sends over
 #  the python script file to the remote machine and executes
@@ -131,17 +153,18 @@ done
 optimize_dbs()
 {
   host=${4//[$'\t\r\n']}
-  remote_cmd="python ${1}/${2} ${3}"
-  if ! ping -c 1 -W 1 "$host" &> /dev/null; then
-    echo "ERROR! Remote host \"$host\" is not responding!"
-  else
+  remote_cmd="python ${1}/${2} ${3} -x $host}"
+  if ping -c 1 -W 1 "$host" &> /dev/null; then
+    echo "$host - ALIVE, executing script..."
     pscp -l admin -pw admin "$PWD/${2}" "$host":"${1}" &> /dev/null
     plink -no-antispoof -l admin -pw admin "$host" "$remote_cmd"
+  else
+    echo "$host - Not Responding!"
   fi
 }
 export -f optimize_dbs
 
-# --------------------------------------------------------------------------------  #
+# ---------------------------------------------------------------------------------  #
 echo "==================== OPTIMIZING DATABASES ===================="
 test="                            DRY RUN                           "
 for opt in "$@"; do
@@ -150,7 +173,7 @@ done
 echo
 
 start_time=$(date -u +"%s.%N")
-parallel optimize_dbs {} ::: "$remote_path" ::: "$remote_file" ::: "${*}" :::: "$PWD/$hosts_file"
+parallel --linebuffer optimize_dbs {} ::: "$remote_path" ::: "$python_file" ::: "${*}" :::: "$PWD/$hosts_file"
 end_time=$(date -u +"%s.%N")
 diff=$(date -u -d "0 $end_time sec - $start_time sec" +"%M min : %S sec")
 
